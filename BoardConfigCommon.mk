00d2d8c65ab89d427cfabf787b692907bb98a268
Sreepad M <84519426+sreepadmarat@users.noreply.github.com>
2026-09-18 21:50:00 +0530

    Add TARGET_KERNEL_VERSION to BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,6 +95,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6375
 TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/debugfs.config
+TARGET_KERNEL_VERSION := 5.4
 TARGET_KERNEL_NO_GCC := true
 
 # Kernel modules
